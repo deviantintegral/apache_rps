a05b8365bc5f5662172b749ec37d20cd9952e8a9
Andrew Berry <deviantintegral@gmail.com>
2012-06-19 14:14:45 -0400

    Add descriptions of each drawing action.

--- rps_daygraph.r
+++ rps_daygraph.r
@@ -19,20 +19,32 @@ ma <- function(x, n=5) {
 # Read in the CSV.
 rps <- read.csv(file=csv, head=TRUE, sep=",");
 
+# Generate a PNG image.
 png(filename=paste(csv, ".png", sep=""), width=800, height=600, units="px", pointsize = 12);
 
+# Initialize the plot.
 plot(rps$requests_per_second, col="darkblue", xlab="Time", ylab="Requests", xaxt="n", type="h");
+
+# Add a line for the 5 second average.
 a <- ma(rps$requests_per_second, 5);
 lines(a, col="green", type="h");
+
+# Add a line for the 60 second average.
 a <- ma(rps$requests_per_second, 60);
 lines(a, col="red", type="h");
+
+# Add a line for the 1 hour average.
 a <- ma(rps$requests_per_second, 3600);
 lines(a, col="yellow", type="h");
+
+# Add a legend to clarify each line.
 legend("topleft", c("Per second", "Per 5 seconds", "Per minute", "Per hour"), col=c("darkblue", "green", "red", "yellow"), lty=1, lwd=2);
 
+# Determine our X axis ticks.
 hours <- rps$date[seq(1, length(rps$date), 3600)];
 axis(1, at=seq(0, 3600*23, 3600), hours);
 
+# Draw a horizontal line showing where our maximum simulataneous capacity is.
 abline(max_clients, 0);
 dev.off();
 
